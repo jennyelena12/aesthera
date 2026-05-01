a0a77f7ffac4680ebedf77bff63fb413e085e855
Ahingg <xaviero.yamin28@gmail.com>
2026-05-01 20:23:52 +0700

    init: project structures

--- aestheraApp/Models.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Models.swift
-//  aestheraApp
-//
-//  Created by Elena Nathanielle on 01/05/26.
-//
-
--- aestheraApp/ViewModels.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Untitled.swift
-//  aestheraApp
-//
-//  Created by Elena Nathanielle on 01/05/26.
-//
-
