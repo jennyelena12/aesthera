872cbf6dbf069699d5af7894ef9f88bee1d535e9
Ahingg <xaviero.yamin28@gmail.com>
2026-05-06 21:38:40 +0700

    fix: large resolution image cannot be processed

--- aestheraApp/ViewModels/FaceScannerViewModel.swift
+++ aestheraApp/ViewModels/FaceScannerViewModel.swift
@@ -14,12 +14,14 @@ class FaceScannerViewModel {
     var detectionState: FaceDetectionState = .idle
     
     func processSelectedImage(_ image: UIImage) {
-        self.selectedImage = image
+        self.selectedImage = compressImageRes(image)
+//        print("New Size: \(selectedImage?.size.width) ,  \(selectedImage?.size.height)")
         self.detectionState = .analyzing
         
+        guard let img = self.selectedImage else {return}
         Task {
             do {
-                let results = try await FaceDetectorService.instance.analyzeImage(image)
+                let results = try await FaceDetectorService.instance.analyzeImage(img)
                 
                 if results.isEmpty {
                     self.detectionState = .noFaceDetected
@@ -31,6 +33,26 @@ class FaceScannerViewModel {
                 self.detectionState = .error(error.localizedDescription)
             }
         }
+    }
+    
+    private func compressImageRes(_ img: UIImage) -> UIImage {
+        let size = img.size
+        let targetDim: CGFloat = 1024.0;
+        
+        let currDim = max(size.width, size.height)
+        if(targetDim > currDim) {return img;}
+        
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1.0;
+        let scale = targetDim / currDim;
+        let newSize = CGSize(width: size.width * scale, height: size.height * scale)
+        
+        let render = UIGraphicsImageRenderer(size: newSize, format: format)
+        
+        return render.image { _ in
+            img.draw(in: CGRect(origin: .zero, size: newSize));
+        }
+        
     }
     
     func reset() {
